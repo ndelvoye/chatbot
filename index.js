27f7ad407b72fd0c5b1ba3e49e59d340492a782d
ndelvoye <nicolhas.delvoye@gmail.com>
2020-12-09 17:23:14 +0100

    First version of the chatbot with RiveScript

---
+++ index.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const express = require('express');
+var RiveScript = require("rivescript");
+
+const PORT = process.env.PORT || 3000;
+const INDEX = '/index.html';
+
+const app = express();
+app.disable("x-powered-by")
+app.use(express.static('public'))
+app.use('/css', express.static(__dirname + '/css'))
+app.use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+const server = app.listen(PORT, () => {
+    console.log(`Listening on ${PORT}`)
+});
+
+// Init RiveScript
+const bot = new RiveScript();
+bot.loadDirectory("brain").then(loading_done).catch(loading_error);
+
+// InitSocketIo
+const io = require('socket.io')(server);
+
+// Gestion des clients Socket.IO
+io.on('connection', (socket) => {
+    let client_name = "Client #" + socket.id + " (IP = " + socket.request.connection.remoteAddress + ")"
+    socket.emit('msg', 'Hello ' + client_name + ' ! Feel free to ask me questions. Type "help" if you need some of it !')
+    console.log(client_name + ' connected');
+    socket.on('ask', (msg) => {
+        console.log(client_name + ' asked : ' + msg)
+        bot.reply("default", msg).then(function(reply) {
+            console.log('Answered back to ' + client_name + ': ' + reply)
+            socket.emit('msg', reply)
+        });
+    })
+    socket.on('disconnect', () => console.log(client_name + ' disconnected'));
+});
+
+// Gestion de la date
+setInterval( () => io.emit('time', new Date().toTimeString()), 1000)
+
+// Fonctions
+function loading_done() {
+    console.log("Bot has finished loading!");
+    bot.sortReplies();
+}
+
+function loading_error(error, filename, lineno) {
+    console.log("Error when loading files: " + error);
+}
+
